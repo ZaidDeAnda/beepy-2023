0a6ba0411ac4fe12ed2375c0db1db98dca3000a2
ZaidDeAnda <zaidthejedi@gmail.com>
2023-05-20 00:42:38 -0600

    added test script

--- test.py
+++ test.py
@@ -1,5 +1,15 @@
 import requests
 
-response = requests.get("http://localhost:8000/")
+url = 'http://localhost:8000/predict'
+image_path = 'gatos.jpg'  # Ruta de la imagen que deseas enviar
 
-print(response.json())
+with open(image_path, 'rb') as file:
+    response = requests.post(url, files={'file': file})
+
+print(response)
+prediction = response.json()
+print(prediction)
+if 'cat' in prediction['prediction']:
+    print(f"En la imagen hay un gato :D")
+else:
+    print("No hay un gato :(")
